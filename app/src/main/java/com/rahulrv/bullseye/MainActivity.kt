60c7bbeaba1093e8fb65d547dd00e2ecd7797e72
Rahul Rengaramanujam <rahul.rv87@gmail.com>
2024-12-28 23:46:22 -0800

    Setting orientation to landscape

--- app/src/main/java/com/rahulrv/bullseye/MainActivity.kt
+++ app/src/main/java/com/rahulrv/bullseye/MainActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -104,7 +105,7 @@ fun GameScreen() {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = Devices.AUTOMOTIVE_1024p, widthDp = 864, heightDp = 432)
 @Composable
 fun GreetingPreview() {
     BullseyeTheme {
